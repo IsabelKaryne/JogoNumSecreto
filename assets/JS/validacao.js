6475ae02efa63ef57da26cee7bc14a2d1ea3d631
IsabelKaryne <isabelksc082@gmail.com>
2023-07-19 12:46:26 -0300

    Subindo finalização da validação

--- assets/JS/validacao.js
+++ assets/JS/validacao.js
@@ -2,12 +2,34 @@ function verificaSeChutePossuiValorInvalido(chute){
     const numero = +chute
 
     if(chuteInvalido(numero)){
-        console.log('Valor inválido')
+        elementoChute.innerHTML += 
+        '<div> valor inválido </div>'
+        return
     }
 
     if(numeroMaiorOuMenorQueValorPermitido (numero)){
-        console.log(`Valor Inválido : O número secreto precisa estar entre ${menorValor} e o ${maiorValor}`)
+        elementoChute.innerHTML += `
+        <div>
+            Valor Inválido : Fale um número entre ${menorValor} e o ${maiorValor}
+        </div>`
+        return
     }
+
+    if(numero === numeroSecreto){
+        document.body.innerHTML = `
+        <h2> Você acertou!</h2>
+        <h3>O número secreto era ${numeroSecreto}</h3>
+        `
+    }else if(numero > numeroSecreto){
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-sharp fa-solid fa-arrow-down"></i></div>
+        `
+    }else{
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-sharp fa-solid fa-arrow-up"></i></div>
+        `
+    }
+
 }
 
 function chuteInvalido(numero){
